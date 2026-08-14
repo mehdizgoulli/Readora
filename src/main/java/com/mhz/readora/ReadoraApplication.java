f337dc14c25e036f3c1e95c899998154d61fb3f5
mehdi zgoulli <mehdizgoulli1@gmail.com>
2026-08-14 21:23:35 +0000

    Initial commit

---
+++ src/main/java/com/mhz/readora/ReadoraApplication.java
@@ -0,0 +1,13 @@
+package com.mhz.readora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhz/readora/ReadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhz.readora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
