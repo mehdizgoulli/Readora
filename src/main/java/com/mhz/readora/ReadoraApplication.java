50e2b8ae913632d514a35f6301b2bec69eb1e33a
mehdi zgoulli <mehdizgoulli1@gmail.com>
2026-08-18 16:15:22 +0000

    authentification

--- src/main/java/com/mhz/readora/ReadoraApplication.java
+++ src/main/java/com/mhz/readora/ReadoraApplication.java
@@ -2,8 +2,11 @@ package com.mhz.readora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ReadoraApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/mhz/readora/security/UserDetailsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.mhz.readora.security;
+
+import com.mhz.readora.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository repository;
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
